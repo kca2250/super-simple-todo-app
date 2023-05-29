f701a9c1b37af7a543eb9c215ab170bf3ad84d74
kca2250 <2250katok@gmail.com>
2023-05-29 20:32:53 +0900

    スーパーシンプルトゥドゥアプリ

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	let newTodo = '';
+	let todos: { id: number; text: string }[] = [];
+	function addTodo() {
+		if (newTodo) {
+			todos = [...todos, { id: Date.now(), text: newTodo }];
+			newTodo = '';
+		}
+	}
+	function deleteTodo(id: number) {
+		todos = todos.filter((todo) => todo.id !== id);
+	}
+</script>
+
+<div>
+	<input
+		bind:value={newTodo}
+		placeholder="New task"
+		on:keydown={(e) => e.key === 'Enter' && addTodo()}
+	/>
+	<button on:click={addTodo}>Add task</button>
+</div>
+<ul>
+	{#each todos as todo (todo.id)}
+		<li>
+			{todo.text}
+			<button on:click={() => deleteTodo(todo.id)}>Delete</button>
+		</li>
+	{/each}
+</ul>
